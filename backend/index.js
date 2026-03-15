c4bd600c3528d9cb47bc24140e27f935477b7390
Manmohan <manmohanshukla815@example.com>
2026-03-15 18:45:18 +0530

    filename_change

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const cors= require('cors');
 
 
-const authRoute= require('./route/authRoutes')
+const authRoute= require('./route/auth.route')
 
 const app = express()
 const PORT = process.env.PORT||3000
